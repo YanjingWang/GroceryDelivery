5f3ca523571ce4f73aa6c4d0f35015119ecc244c
xli3037 <xli3037@gatech.edu>
2022-04-09 22:35:01 -0400

    log4j files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,16 @@ import edu.gatech.cs6310.DeliveryService;
 
 import static edu.gatech.cs6310.DBManager.showData;
 
+import edu.gatech.cs6310.DeliveryServiceUtility;
+
 public class Main {
+
+
     public static void main(String[] args) {
         System.out.println("Welcome to the Grocery Express Delivery Service!");
 //        showData();
+
+
         DeliveryService simulator = new DeliveryService();
         simulator.commandLoop();
         System.out.println("simulation terminated");
