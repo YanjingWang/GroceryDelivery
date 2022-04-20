2a249194ce8d13d3985cb03e684fb7c955f9a832
MingLi1282 <ming.li.hailin@gmail.com>
2022-04-19 20:28:26 -0400

    04192022 Ming sellItem

--- src/main/java/edu/gatech/cs6310/Controller.java
+++ src/main/java/edu/gatech/cs6310/Controller.java
@@ -6,8 +6,7 @@ import org.apache.logging.log4j.Logger;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Controller {
     private static DBManager manager = new DBManager();
@@ -53,8 +52,131 @@ public class Controller {
         String name = store.getName();
         Long revenue = store.getRevenue();
         int rs = manager.insert("INSERT INTO delivery.store (store_name, revenue) VALUES ('" + name + "', " + revenue + ")");
-
         return rs == 1;
     }
+
+    public Item findItemByName(String storeName, String itemName) {
+        Item item = null;
+        try (ResultSet rs = manager.get("SELECT * FROM item WHERE `store_name` = '"+ storeName +"' AND `item_name` = '" + itemName+ "'")) {
+            if (rs != null) {
+                rs.next();
+                Long unitWeight = rs.getLong("unit_weight");
+                item = new Item(storeName, unitWeight);
+            }
+        } catch(SQLException e) {
+            logger.error("Error find item by name: " + itemName + e);
+        }
+        return item;
+    }
+
+    public boolean createNewItem(String storeName, Item item){
+        String itemName = item.getItemName();
+        Long weight = item.getWeight();
+        int rs = manager.insert("INSERT INTO delivery.store(item_name, unit_weight, store_name) VALUES ('" + itemName + "', " + weight + ",'" + storeName + "')");
+        return rs == 1;
+    }
+
+    public TreeMap<String,Item>  findAllItem(String storeName) {
+        TreeMap<String,Item> items = new TreeMap<String,Item>();
+        try (ResultSet rs = manager.get("SELECT * FROM item WHERE `store_name` = '"+ storeName +"'")) {
+            if (rs != null) {
+                while (rs.next()) {
+                    String itemName = rs.getString("item_name");
+                    Long itemWeight = rs.getLong("unit_weight");
+                    items.put(itemName,new Item(itemName,itemWeight));
+                }
+            }
+        } catch(SQLException e) {
+            System.out.println("Error find all items. " + e);
+            logger.error("Error find all items. " + e);
+        } finally {
+            manager.closeConnection();
+        }
+        return items;
+    }
+
+    public Pilot findPilotByID(String accountID) {
+        Pilot pilot = null;
+        User user =null;
+        try (ResultSet rs = manager.get("SELECT * " +
+                "FROM `pilot` AS s" +
+                "INNER JOIN `user` AS u" +
+                "ON s.pilot_id = u.account_id" +
+                "WHERE s.pilot_id ='" + accountID + "'")) {
+            if (rs != null) {
+                rs.next();
+                String firstName = rs.getString("firstname");
+                String lastName = rs.getString("lastname");
+                String phoneNo = rs.getString("phonenumber");
+                user = new User(firstName,lastName,phoneNo);
+
+                String accountId = rs.getString("pilot_id");
+                String taxId = rs.getString("tax_id");
+                String licenseId = rs.getString("license_id");
+                Integer experience = rs.getInt("experience");
+                pilot = new Pilot(accountId, user, taxId,licenseId,experience);
+            }
+        } catch(SQLException e) {
+            logger.error("Error find store by account: " + accountID + e);
+        }
+        return pilot;
+    }
+
+    public Set<String>  findAllLicense() {
+        Set<String> licenses = new HashSet<String>();
+        try (ResultSet rs = manager.get("SELECT `license_id` FROM `pilot`")) {
+            if (rs != null) {
+                while (rs.next()) {
+                    String licenseID = rs.getString("license_id");
+                    licenses.add(licenseID);
+                }
+            }
+        } catch(SQLException e) {
+            System.out.println("Error find all licenes. " + e);
+            logger.error("Error find all licenes. " + e);
+        } finally {
+            manager.closeConnection();
+        }
+        return licenses;
+    }
+
+    public TreeMap<String,Pilot> findAllPilot(){
+        TreeMap<String, Pilot> pilots = new TreeMap<>();
+        try (ResultSet rs = manager.get("SELECT * FROM `pilot` AS s INNER JOIN `user` AS u ON s.pilot_id = u.account_id")) {
+            if (rs != null) {
+                while (rs.next()) {
+                    String firstName = rs.getString("firstname");
+                    String lastName = rs.getString("lastname");
+                    String phoneNo = rs.getString("phonenumber");
+                    String accountId = rs.getString("pilot_id");
+                    String taxId = rs.getString("tax_id");
+                    String licenseId = rs.getString("license_id");
+                    Integer experience = rs.getInt("experience");
+                    pilots.put(accountId, new Pilot(accountId, new User(firstName, lastName,phoneNo), taxId, licenseId, experience));
+                }
+            }
+        } catch(SQLException e) {
+            System.out.println("Error find all pilots. " + e);
+            logger.error("Error find all pilots. " + e);
+        } finally {
+            manager.closeConnection();
+        }
+        return pilots;
+    }
+
+    public boolean createNewPilot(Pilot pilot) {
+        String accountId = pilot.getAccountId();
+        String firstName = pilot.getUser().getFirstName();
+        String lastName = pilot.getUser().getLastName();
+        String phoneNo = pilot.getUser().getPhoneNumber();
+        String taxId = pilot.getTaxId();
+        String licenseId = pilot.getLicenseId();
+        Integer experience = pilot.getExperience();
+        String password = "123";
+        int rs_pilot = manager.insert("INSERT INTO delivery.pilot (`pilot_id`,`tax_id`,`license_id`, `experience`) VALUES ('" + accountId + "', '" + taxId + "', '" + licenseId + "', " + experience + ")");
+        int rs_user = manager.insert("INSERT INTO delivery.pilot(`account_id`,`password`,`firstname`, `lastname`, `phonenumebr`) VALUES('" + accountId + "', '" + password + "', '" + firstName + "', '" + lastName + "','" + phoneNo + "',)");
+        return rs_pilot == 1;
+    }
+
 
 }
